8d1390754cfd61147a71ce12a707edc4b20bbd27
Mauro Bianchi <bianchimro@gmail.com>
2018-11-21 16:25:08 +0100

    fixing run in docker

--- index.js
+++ index.js
@@ -18,15 +18,25 @@ const asyncMiddleware = fn =>
     return
   }
 
-  const { width, height, printBackground=true, fileName } =
+  const { width, height, printBackground=true, fileName } = req.query
 
   const browser = await puppeteer.launch({
+    args: ['--no-sandbox'],
     userDataDir: './.pdfcache',
-    dumpio: true
+    // dumpio: true
   })
   const page = await browser.newPage()
+  try {
+    await page.goto(url, {waitUntil: 'networkidle2'})
+  } catch(err) {
+    res.statusCode = 400;
+    res.send(err)
+    return
+  }
 
-  await page.goto(url, {waitUntil: 'networkidle2'})
+  const status = page.headers.status
+  console.log("page status", status)
+
   const title = await page.title()
   const pdfFileName = fileName ? fileName : title.toLowerCase().replace(/ /g, '_') + '.pdf'
   await page.pdf({
@@ -35,6 +45,8 @@ const asyncMiddleware = fn =>
     height,
     printBackground: !!printBackground
   });
+
+  await page.close();
   await browser.close();
   res.download(__dirname + '/' + pdfFileName, pdfFileName);
 }
