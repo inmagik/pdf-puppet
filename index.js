3907b0272d055b36526ad5f9218ec24126262b6d
Mauro Bianchi <bianchimro@gmail.com>
2018-11-22 12:28:38 +0100

    passing auth headers

--- index.js
+++ index.js
@@ -18,32 +18,36 @@ const asyncMiddleware = fn =>
   };
 
 
-async function getPage(url, viewport){
+async function getPage(url, options={}){
   const browser = await puppeteer.launch({
     args: ['--no-sandbox'],
     userDataDir: './.pdfcache',
     // dumpio: true
   })
   const page = await browser.newPage()
-  if(viewport){
+  if(options.viewport){
     await page.setViewport({
-      width: viewport.width,
-      height: viewport.height,
+      width: options.viewport.width,
+      height: options.viewport.height,
     })
   }
+  if(options.headers){
+    await page.setExtraHTTPHeaders(options.headers)
+  }
+
   await page.goto(url, { waitUntil: 'networkidle2'})
   const title = await page.title()
   return { page:page, browser:browser, title:title }
 }
 
 async function urlToPdf(req, res){
+
   const url = req.query.url
   if(!url){
     res.statusCode = 400;
     res.send("No url specified!")
     return
   }
-
   const {
     width, height,
     printBackground=true,
@@ -54,8 +58,9 @@ async function urlToPdf(req, res){
 
   var result;
   try {
-    result = await getPage(url)
+    result = await getPage(url, { headers: { authorization: req.headers['authorization']}, })
   } catch(err) {
+    console.error(err)
     res.statusCode = 404;
     res.send(err)
     return
@@ -63,7 +68,7 @@ async function urlToPdf(req, res){
 
   const { page, title, browser } = result
 
-  const pdfFileName = fileName ? fileName : title.toLowerCase().replace(/ /g, '_') + '.pdf'
+  const pdfFileName = fileName ? fileName : (title || 'No title').toLowerCase().replace(/ /g, '_') + '.pdf'
   const path = output_folder + '/' + pdfFileName
   await page.pdf({
     path,
@@ -77,10 +82,13 @@ async function urlToPdf(req, res){
 
   await page.close();
   await browser.close();
+
   res.download(
     path,
     pdfFileName,
-    () => { fs.unlinkSync(path); }
+    () => {
+      fs.unlinkSync(path);
+    }
   )
 }
 
@@ -97,10 +105,18 @@ async function urlToScreenshot(req, res){
     viewportWidth=1440,
     viewportHeight=900,
   } = req.query
+
   let result
   try {
-    result = await getPage(url, { width: viewportWidth, height: viewportHeight})
+    result = await getPage(
+      url,
+      {
+        headers: { authorization: req.headers['authorization'] },
+        viewport: { width: viewportWidth, height: viewportHeight},
+      }
+    )
   } catch(err) {
+    console.error(err)
     res.statusCode = 404;
     res.send(err)
     return
