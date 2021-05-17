e15505dcfb3ea3b11996d932ea686c7ead11dab8
gffuma <fumagalli.gf@gmail.com>
2021-05-17 12:28:19 +0200

    Switch to new registry and consider actual version v2 stable

--- build_image.sh
+++ build_image.sh
@@ -1,6 +1,3 @@
-#docker build -t gitlab.inmagik.com:4443/inmagik/images/pdf-puppet-dev .
-#docker push gitlab.inmagik.com:4443/inmagik/images/pdf-puppet-dev
-
 tsc
-docker build -t registry.inmagik.com/pdf-puppet/pdf-puppet:edge .
-# docker push registry.inmagik.com/pdf-puppet/pdf-puppet:edge
+docker build -t docker.inmagik.com/pdf-puppet/pdf-puppet .
+docker push docker.inmagik.com/pdf-puppet/pdf-puppet
